8a34c5df900f9f033cebedd791d185f736eb694a
Michel Smola <michel.smola@farbenmeer.de>
2025-08-19 20:24:45 +0200

    feat: local client

--- src/create-fetch-client.ts
+++ src/create-fetch-client.ts
@@ -1,6 +1,6 @@
 import type { MaybePromise } from "bun";
+import type { Client } from "./client";
 import { handleResponse } from "./handle-response";
-import type { Handler as BaseHandler } from "./handler";
 import type { Path as BasePath } from "./path";
 import type { BaseRoute } from "./route";
 
@@ -75,61 +75,3 @@ function createProxy(baseUrl: string, options: Options, lastProp: string) {
     },
   });
 }
-
-type Segment<Path> = Path extends `/${infer Segment}/${string}`
-  ? Segment
-  : Path extends `/${infer Segment}`
-    ? Segment
-    : never;
-
-type Rest<
-  Path,
-  Segment extends string,
-> = Path extends `/${Segment}/${infer Rest}` ? `/${Rest}` : never;
-
-type Client<Routes extends Record<BasePath, MaybePromise<BaseRoute>>> = {
-  [segment in Segment<keyof Routes> as segment extends `[${string}]`
-    ? string
-    : segment]: (Extract<keyof Routes, `/${segment}`> extends never
-    ? {}
-    : ClientRoute<Routes[`/${segment}`]>) &
-    (Exclude<keyof Routes, `/${segment}`> extends never
-      ? {}
-      : Client<{
-          [rest in Rest<keyof Routes, segment>]: Routes[`/${segment}${rest}`];
-        }>);
-};
-
-type ClientRoute<Route extends MaybePromise<BaseRoute>> = {
-  get: RouteWithoutBody<Awaited<Route>["get"]>;
-  post: RouteWithBody<Awaited<Route>["post"]>;
-};
-
-type RouteWithoutBody<
-  Handler extends BaseHandler<any, any, any, never> | undefined,
-> = Handler extends undefined
-  ? never
-  : keyof QueryType<Handler> extends never
-    ? (query?: {}, req?: RequestInit) => ResponseType<Handler>
-    : (query: QueryType<Handler>, req?: RequestInit) => ResponseType<Handler>;
-
-type RouteWithBody<
-  Handler extends BaseHandler<any, any, any, unknown> | undefined,
-> = Handler extends undefined
-  ? never
-  :
-      | ((
-          query: QueryType<Handler>,
-          body: BodyType<Handler> | FormData,
-          req?: RequestInit,
-        ) => ResponseType<Handler>)
-      | ((body: FormData) => ResponseType<Handler>);
-
-type QueryType<Handler extends { schema: { __q?: any } } | undefined> =
-  NonNullable<NonNullable<Handler>["schema"]["__q"]>;
-
-type BodyType<Handler extends { schema: { __b?: any } } | undefined> =
-  NonNullable<NonNullable<Handler>["schema"]["__b"]>;
-
-type ResponseType<Handler extends { schema: { __r?: any } } | undefined> =
-  Promise<NonNullable<NonNullable<Handler>["schema"]["__r"]>>;
---
+++ src/create-local-client.ts
@@ -0,0 +1,18 @@
+import type { MaybePromise } from "bun";
+import type { ApiDefinition } from "./define-api";
+import type { Path } from "./path";
+import type { BaseRoute } from "./route";
+import { createFetchClient } from "./create-fetch-client";
+import { createRequestHandler } from "./create-request-handler";
+
+export function createLocalClient<
+  Routes extends Record<Path, MaybePromise<BaseRoute>>,
+>(api: ApiDefinition<Routes>) {
+  const handler = createRequestHandler(api);
+  return createFetchClient<Routes>("http://localhost", {
+    fetch(url, init) {
+      const req = new Request(url, init);
+      return handler(req);
+    },
+  });
+}
