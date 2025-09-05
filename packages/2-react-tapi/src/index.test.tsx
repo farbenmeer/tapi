5c99d5f7f40aac3323b0173a542c0e727860c56e
Michel Smola <michel.smola@farbenmeer.de>
2025-09-05 23:43:20 +0200

    test: add tests for react-tapi

---
+++ packages/2-react-tapi/src/index.test.tsx
@@ -0,0 +1,75 @@
+import {
+  createLocalClient,
+  defineApi,
+  defineHandler,
+  TResponse,
+} from "@farbenmeer/tapi/server";
+import { act, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test } from "bun:test";
+import { Suspense } from "react";
+import { z } from "zod/v4";
+import { useQuery } from ".";
+
+describe("useQuery", () => {
+  const api = defineApi()
+    .route("/noQuery", {
+      GET: defineHandler(
+        {
+          authorize: () => true,
+        },
+        async (req) => {
+          return TResponse.json({ message: "No Query" });
+        }
+      ),
+    })
+    .route("/withQuery", {
+      GET: defineHandler(
+        {
+          authorize: () => true,
+          query: {
+            q: z.string(),
+          },
+        },
+        async (req) => {
+          const { q } = req.query();
+          return TResponse.json({ message: `Query: ${q}` });
+        }
+      ),
+    });
+
+  const client = createLocalClient(api);
+
+  test("no query", async () => {
+    function Sut() {
+      const data = useQuery(client.noQuery.get());
+      return <div>{data.then((data) => data.message)}</div>;
+    }
+
+    await act(() =>
+      render(
+        <Suspense fallback={<div>Loading...</div>}>
+          <Sut />
+        </Suspense>
+      )
+    );
+
+    expect(screen.getByText("No Query")).toBeInTheDocument();
+  });
+
+  test("With Query", async () => {
+    function Sut() {
+      const data = useQuery(client.withQuery.get({ q: "test" }));
+      return <div>{data.then((data) => data.message)}</div>;
+    }
+
+    await act(() =>
+      render(
+        <Suspense fallback={<div>Loading...</div>}>
+          <Sut />
+        </Suspense>
+      )
+    );
+
+    expect(screen.getByText("Query: test")).toBeInTheDocument();
+  });
+});
