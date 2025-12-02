35472edd25fdaf5ab3c44aaae99b248c2ea61e18
Michel Smola <michel.smola@farbenmeer.de>
2025-12-02 11:14:41 +0100

    refactor: lacy returns thenable, not promise
    also add the '$'-operator to turn it into a real promise

--- packages/1-lacy/src/index.ts
+++ packages/1-lacy/src/index.ts
@@ -1,16 +1,30 @@
+type Thenable<T> = {
+  then<R = T>(
+    onfulfilled?: (value: T) => R | PromiseLike<R>,
+    onrejected?: (reason: any) => void
+  ): Promise<R>;
+  catch(onrejected: (reason: any) => void): Promise<T>;
+  finally(onfinally: () => void): Promise<T>;
+  $: Promise<T>;
+};
+
 export type LacyPromise<T, P = never> = T extends (...args: infer A) => infer R
   ? (...args: A) => LacyPromise<R>
   : {
-      [key in Exclude<keyof T, "then" | "catch" | "finally">]: LacyPromise<
-        T[key]
-      >;
-    } & Promise<T | P>;
+      [key in Exclude<
+        keyof T,
+        "then" | "catch" | "finally" | "$"
+      >]: LacyPromise<T[key]>;
+    } & Thenable<T | P>;
 
 export function lacy<T>(data: Promise<T>): LacyPromise<T> {
   return new Proxy(() => data, {
     get(target, prop): any {
       if (prop === "then" || prop === "catch" || prop === "finally") {
         return (...args: any) => (target()[prop] as Function)(...args);
+      }
+      if (prop === "$") {
+        return target();
       }
 
       return lacy<unknown>(
@@ -26,5 +40,5 @@ export function lacy<T>(data: Promise<T>): LacyPromise<T> {
     apply(target, _thisArg, argArray) {
       return lacy<unknown>(target().then((method: any) => method(...argArray)));
     },
-  }) as LacyPromise<T>;
+  }) as unknown as LacyPromise<T>;
 }
