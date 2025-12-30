9526ca0f49a8eb5c31353c4be2538d3ce30ca784
Michel Smola <michel.smola@farbenmeer.de>
2025-12-30 23:14:42 +0100

    fix: standalone server

---
+++ .changeset/slick-sites-repeat.md
@@ -0,0 +1,5 @@
+---
+"@farbenmeer/bunny": patch
+---
+
+fix \_\_dirname and incorrect api import in server.js
--- packages/3-bunny/src/cli/generate-server.ts
+++ packages/3-bunny/src/cli/generate-server.ts
@@ -1,12 +1,12 @@
 export function generateServer() {
   return `
-import api from "./api.cjs"
+import { api } from "./api.cjs"
 import { startBunnyServer } from "@farbenmeer/bunny/server"
 
 startBunnyServer({
   port: 3000,
   api,
-  dist: __dirname + "/dist",
+  dist: import.meta.dirname + "/dist",
 });
 `;
 }
