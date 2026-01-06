03c9103b4f9eaec84a8035d55795f74b849951af
Michel Smola <michel.smola@farbenmeer.de>
2026-01-06 15:30:52 +0100

    feat: export HttpError from client package

--- packages/1-tapi/src/client/index.ts
+++ packages/1-tapi/src/client/index.ts
@@ -1,3 +1,4 @@
 export type { Observable } from "./client-types.js";
 export { createFetchClient } from "./create-fetch-client.js";
 export type { GetRoute, PostRoute } from "./route-types.js";
+export { HttpError } from "../shared/http-error.js";
