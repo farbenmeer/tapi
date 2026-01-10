e2ffcfa907a231ce1186313b9e88d8863780e536
Michel Smola <michel.smola@farbenmeer.de>
2026-01-10 18:18:44 +0100

    feat: full todolist-example

---
+++ examples/todo-list/src/api/todo.ts
@@ -0,0 +1,38 @@
+import { defineHandler, TResponse } from "@farbenmeer/bunny/server";
+import { db } from "db";
+import z from "zod";
+
+const params = {
+  id: z.string(),
+};
+
+export const DELETE = defineHandler(
+  {
+    params,
+    authorize: () => true,
+  },
+  async (req) => {
+    db.prepare("DELETE FROM todos WHERE id = ?").run(req.params().id);
+
+    return TResponse.void({ tags: ["todos"] });
+  }
+);
+
+export const PATCH = defineHandler(
+  {
+    params,
+    authorize: () => true,
+    body: z.object({
+      done: z.stringbool().optional(),
+    }),
+  },
+  async (req) => {
+    const { done } = await req.data();
+    db.prepare("UPDATE todos SET done = ? WHERE id = ?").run(
+      done ? 1 : 0,
+      req.params().id
+    );
+
+    return TResponse.void({ tags: ["todos"] });
+  }
+);
