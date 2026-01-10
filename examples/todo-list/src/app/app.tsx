aec759a2bfb3d60bd10496f6c3972ed8aa9650ed
Michel Smola <michel.smola@farbenmeer.de>
2026-01-10 15:13:43 +0100

    refactor: remove unused imports

--- examples/todo-list/src/app/app.tsx
+++ examples/todo-list/src/app/app.tsx
@@ -1,6 +1,5 @@
-import { useLacy, useQuery } from "@farbenmeer/bunny/client";
+import { useQuery } from "@farbenmeer/bunny/client";
 import { client } from "client";
-import { Suspense } from "react";
 
 export function App() {
   const todos = useQuery(client.todos.get());
