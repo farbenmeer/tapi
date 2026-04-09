8e9abfcdedd1621b9e913d70dfc8d2b551da9fed
Michel Smola <michel.smola@farbenmeer.de>
2026-04-09 22:34:00 +0200

    feat: workflow postgres adapter

---
+++ packages/1-workflow/src/adapter-drizzle-better-sqlite/adapter.test.ts
@@ -0,0 +1,70 @@
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { startEngine } from "../engine";
+import { step } from "../step";
+import { workflow } from "../workflow";
+import { DrizzleBetterSqliteAdapter } from "./adapter";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+
+const SCHEMA = readFileSync(
+  path.resolve(import.meta.dirname, "../sql/sqlite.sql"),
+  "utf-8",
+);
+
+describe("DrizzleBetterSqliteAdapter", () => {
+  let drizzleDb: BetterSQLite3Database;
+
+  beforeEach(() => {
+    const sqliteDb = new Database(":memory:");
+    sqliteDb.exec(SCHEMA);
+    drizzleDb = drizzle(sqliteDb);
+  });
+
+  test("Basic adapter methods", async () => {
+    const adapter = new DrizzleBetterSqliteAdapter(drizzleDb);
+
+    await adapter.createWorkflow({
+      workflowId: "test",
+      input: {},
+    });
+
+    expect(await adapter.getNextWorkflow(300)).toEqual({
+      workflowId: "test",
+      runId: expect.any(String),
+      error: null,
+      input: {},
+      leaseExpiredAt: expect.any(Date),
+      startedAt: expect.any(Date),
+      finishedAt: null,
+    });
+  });
+
+  test("basic workflow run", async () => {
+    const cb = vi.fn();
+
+    const helloStep = step(async (input: string) => {
+      return `Hello, ${input}!`;
+    });
+
+    const returnStep = step(async (input: string) => {
+      cb(input);
+    });
+
+    const engine = startEngine({
+      storage: new DrizzleBetterSqliteAdapter(drizzleDb),
+      workflows: {
+        test: workflow(function testWorkflow(who: string) {
+          const message = helloStep(who);
+          returnStep(message);
+        }),
+      },
+    });
+
+    await engine.test("world");
+
+    await vi.waitFor(() => expect(cb).toHaveBeenCalledWith("Hello, world!"));
+  });
+});
---
+++ packages/1-workflow/src/sql/sqlite.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS workflow_state (
+  workflow_id TEXT NOT NULL,
+  run_id TEXT PRIMARY KEY NOT NULL,
+  error TEXT,
+  input TEXT,
+  leaseExpiredAt INTEGER NOT NULL,
+  started_at INTEGER NOT NULL,
+  finished_at INTEGER,
+  resume_at INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS workflow_step_state (
+  run_id TEXT NOT NULL,
+  step_id TEXT NOT NULL,
+  result TEXT,
+  error TEXT,
+  attempt INTEGER NOT NULL DEFAULT 0,
+  PRIMARY KEY (run_id, step_id)
+);
