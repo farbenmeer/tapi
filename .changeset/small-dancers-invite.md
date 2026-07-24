8e595c915dfc2bdd4189c34bd86ca50c3a3eb8ea
Michel Smola <michel.smola@farbenmeer.de>
2026-07-24 15:50:31 +0200

    refactor: rename delete api to invalidate

---
+++ .changeset/small-dancers-invite.md
@@ -0,0 +1,5 @@
+---
+"@toapi/cache": minor
+---
+
+Add invalidate API (same functionality as delete), make delete an alias, mark delete as deprecated
--- packages/1-toapi-cache/src/index.ts
+++ packages/1-toapi-cache/src/index.ts
@@ -20,6 +20,12 @@ export interface Cache {
   set(
     input: CacheEntry & { key: string; ttl: number; tags: string[] }
   ): Promise<void>;
+  /** @deprecated use invalidate instead
+   *
+   * invalidate is more idiomatic naming for a cache (and this is clearly a pure cache, not general purpose database).
+   * it is also consistent with the wording used in the tapi server package.
+   */
   delete(tags: string[], meta?: { clientId?: string }): Promise<void>;
+  invalidate(tags: string[], meta?: { clientId?: string }): Promise<void>;
   subscribe(callback: Subscription): () => void;
 }
