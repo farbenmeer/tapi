2840ef79a520a00ef8a2f6d0eba62889d967f0f6
Michel Smola <michel.smola@farbenmeer.de>
2026-08-23 18:18:03 +0200

    fix: serve entries from network that have no metadata

--- packages/toapi-worker/src/handle-toapi-request.ts
+++ packages/toapi-worker/src/handle-toapi-request.ts
@@ -24,25 +24,19 @@ export async function handleToapiRequest(
 
     const meta = await getMetadata(req.url);
 
-    if (meta?.expiresAt) {
-      if (meta.expiresAt > Date.now()) {
-        // cached response is still valid
+    if (meta && (!meta.expiresAt || meta.expiresAt > Date.now())) {
+      return cachedResponse;
+    } else {
+      // cached response is expired
+      try {
+        // try to serve from network
+        return await serveFromNetwork(req);
+      } catch (error) {
+        // probably network not available, serve old response
+        errorLog(error);
         return cachedResponse;
-      } else {
-        // cached response is expired
-        try {
-          // try to serve from network
-          return await serveFromNetwork(req);
-        } catch (error) {
-          // probably network not available, serve old response
-          errorLog(error);
-          return cachedResponse;
-        }
       }
     }
-
-    // no expiration header, serve cached response
-    return cachedResponse;
   }
 }
 
