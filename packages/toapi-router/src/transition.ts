39ce8e88c83df226ea14911664d01c673d1db238
Michel Smola <michel.smola@farbenmeer.de>
2026-09-15 09:59:00 +0200

    feat: explicitly control transition apis

---
+++ packages/toapi-router/src/transition.ts
@@ -0,0 +1,17 @@
+import { startTransition, type TransitionFunction } from "react";
+
+export type UseTransitionParameter =
+  ((scope: TransitionFunction) => void) | boolean;
+
+export function runTransition(
+  p: UseTransitionParameter | undefined,
+  scope: TransitionFunction,
+) {
+  if (p === false) {
+    scope();
+  }
+  if (typeof p === "function") {
+    p(scope);
+  }
+  startTransition(scope);
+}
